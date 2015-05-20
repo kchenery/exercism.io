8f1a858f30dd0d32c439e1ffb5e17be80cae91ae
Kent Chenery <kent@chenery.net.nz>
2015-05-20 23:48:52 +1200

    Directory tidy up.
    Leap class to test for leap year.  This would be nicer as an extension method.

--- csharp/leap/LeapTest.cs
+++ csharp/leap/LeapTest.cs
@@ -9,21 +9,18 @@ public class LeapTest
         Assert.That(Year.IsLeap(1996), Is.True);
     }
 
-    [Ignore]
     [Test]
     public void Invalid_leap_year()
     {
         Assert.That(Year.IsLeap(1997), Is.False);
     }
 
-    [Ignore]
     [Test]
     public void Turn_of_the_20th_century_is_not_a_leap_year()
     {
         Assert.That(Year.IsLeap(1900), Is.False);
     }
 
-    [Ignore]
     [Test]
     public void Turn_of_the_25th_century_is_a_leap_year()
     {
