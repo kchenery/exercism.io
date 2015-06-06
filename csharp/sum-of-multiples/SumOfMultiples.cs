785ee62537af800a0cd47c7fe07ba27bc96c215c
Kent Chenery <kent@chenery.net.nz>
2015-06-06 21:53:22 +1200

    Broke out some recursion!

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism
+{
+    public class SumOfMultiples
+    {
+        private List<int> _multiples;
+
+        public SumOfMultiples(List<int> multiples)
+        {
+            _multiples = multiples;
+        }
+
+        public SumOfMultiples() : this(new List<int> { 3, 5 })
+        {
+        }
+
+        public int To(int upto)
+        {
+            return To(1, upto);
+        }
+
+        private int To(int current, int upto)
+        {
+            if (current == upto)
+                return 0;
+            else
+                return (IsMultiple(current) ? current : 0) + To(current + 1, upto);
+        }
+
+        private bool IsMultiple(int value)
+        {
+            foreach (var multiple in _multiples)
+            {
+                if (value % multiple == 0)
+                    return true;
+            }
+
+            return false;
+        }
+
+    
+    }
+}
