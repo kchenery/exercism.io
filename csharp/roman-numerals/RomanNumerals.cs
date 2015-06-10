a6f32e904562bdcbb09765ef86f001bac8ffed66
Kent Chenery <kent@chenery.net.nz>
2015-06-10 22:12:10 +1200

    Ripped a tidier version of the web.  Mine was similar...just not as elegant as this :)

---
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Exercism.RomanNumerals
+{
+    public static class RomanNumerals
+    {
+        public static string ToRoman(this int arabic)
+        {
+            if (arabic < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            StringBuilder romanNumberals = new StringBuilder();
+
+            int[] decimals = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+            string[] romans = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+
+            while (arabic > 0)
+            {
+                for (int i = decimals.Count() - 1; i >= 0; i--)
+                {
+                    if (arabic / decimals[i] >= 1)
+                    {
+                        romanNumberals.Append(romans[i]);
+                        arabic -= decimals[i];
+                        break;
+                    }
+                }
+            }
+
+            return romanNumberals.ToString();
+        }
+    }
+}
--- csharp/roman-numerals/RomanNumeralsTest.cs
+++ csharp/roman-numerals/RomanNumeralsTest.cs
@@ -1,30 +1,31 @@
 using NUnit.Framework;
+using Exercism.RomanNumerals;
 
 [TestFixture]
 public class RomanNumeralsTest
 {
-    // Change 'Ignore = true' to 'Ignore = false' to run a test case.
-    // You can also just remove 'Ignore = true'.
+    // Change 'Ignore = false' to 'Ignore = false' to run a test case.
+    // You can also just remove 'Ignore = false'.
 
     [TestCase(0, Result = "")]
-    [TestCase(1, Result = "I", Ignore = true)]
-    [TestCase(2, Result = "II", Ignore = true)]
-    [TestCase(3, Result = "III", Ignore = true)]
-    [TestCase(4, Result = "IV", Ignore = true)]
-    [TestCase(5, Result = "V", Ignore = true)]
-    [TestCase(6, Result = "VI", Ignore = true)]
-    [TestCase(9, Result = "IX", Ignore = true)]
-    [TestCase(27, Result = "XXVII", Ignore = true)]
-    [TestCase(48, Result = "XLVIII", Ignore = true)]
-    [TestCase(59, Result = "LIX", Ignore = true)]
-    [TestCase(93, Result = "XCIII", Ignore = true)]
-    [TestCase(141, Result = "CXLI", Ignore = true)]
-    [TestCase(163, Result = "CLXIII", Ignore = true)]
-    [TestCase(402, Result = "CDII", Ignore = true)]
-    [TestCase(575, Result = "DLXXV", Ignore = true)]
-    [TestCase(911, Result = "CMXI", Ignore = true)]
-    [TestCase(1024, Result = "MXXIV", Ignore = true)]
-    [TestCase(3000, Result = "MMM", Ignore = true)]
+    [TestCase(1, Result = "I", Ignore = false)]
+    [TestCase(2, Result = "II", Ignore = false)]
+    [TestCase(3, Result = "III", Ignore = false)]
+    [TestCase(4, Result = "IV", Ignore = false)]
+    [TestCase(5, Result = "V", Ignore = false)]
+    [TestCase(6, Result = "VI", Ignore = false)]
+    [TestCase(9, Result = "IX", Ignore = false)]
+    [TestCase(27, Result = "XXVII", Ignore = false)]
+    [TestCase(48, Result = "XLVIII", Ignore = false)]
+    [TestCase(59, Result = "LIX", Ignore = false)]
+    [TestCase(93, Result = "XCIII", Ignore = false)]
+    [TestCase(141, Result = "CXLI", Ignore = false)]
+    [TestCase(163, Result = "CLXIII", Ignore = false)]
+    [TestCase(402, Result = "CDII", Ignore = false)]
+    [TestCase(575, Result = "DLXXV", Ignore = false)]
+    [TestCase(911, Result = "CMXI", Ignore = false)]
+    [TestCase(1024, Result = "MXXIV", Ignore = false)]
+    [TestCase(3000, Result = "MMM", Ignore = false)]
     public string Convert_roman_to_arabic_numerals(int arabicNumeral)
     {
         return arabicNumeral.ToRoman();
