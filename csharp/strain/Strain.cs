a315d5bb2e3a0b6d01572c71767be2a8bcf8b261
Kent Chenery <kent@chenery.net.nz>
2015-06-12 00:11:13 +1200

    I should read the errors more closely!

---
+++ csharp/strain/Strain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercism.Strain
+{
+    public static class Strain
+    {
+        public static IEnumerable<T> Keep<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate.Invoke(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<T> Discard<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (!(predicate.Invoke(item)))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
--- csharp/strain/StrainTest.cs
+++ csharp/strain/StrainTest.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using Exercism.Strain;
 
 [TestFixture]
 public class StrainTest
@@ -11,28 +12,24 @@ public class StrainTest
         Assert.That(new LinkedList<int>().Keep(x => x < 10), Is.EqualTo(new LinkedList<int>()));
     }
 
-    [Ignore]
     [Test]
     public void Keep_everything()
     {
         Assert.That(new HashSet<int> { 1, 2, 3 }.Keep(x => x < 10), Is.EqualTo(new HashSet<int> { 1, 2, 3 }));
     }
 
-    [Ignore]
     [Test]
     public void Keep_first_and_last()
     {
         Assert.That(new[] { 1, 2, 3 }.Keep(x => x % 2 != 0), Is.EqualTo(new[] { 1, 3 }));
     }
 
-    [Ignore]
     [Test]
     public void Keep_neither_first_nor_last()
     {
         Assert.That(new List<int> { 1, 2, 3, 4, 5 }.Keep(x => x % 2 == 0), Is.EqualTo(new List<int> { 2, 4 }));
     }
 
-    [Ignore]
     [Test]
     public void Keep_strings()
     {
@@ -40,7 +37,6 @@ public class StrainTest
         Assert.That(words.Keep(x => x.StartsWith("z")), Is.EqualTo("zebra zombies zelot".Split(' ')));
     }
 
-    [Ignore]
     [Test]
     public void Keep_arrays()
     {
@@ -58,35 +54,30 @@ public class StrainTest
         Assert.That(actual.Keep(x => x.Contains(5)), Is.EqualTo(expected));
     }
 
-    [Ignore]
     [Test]
     public void Empty_discard()
     {
         Assert.That(new LinkedList<int>().Discard(x => x < 10), Is.EqualTo(new LinkedList<int>()));
     }
 
-    [Ignore]
     [Test]
     public void Discard_nothing()
     {
         Assert.That(new HashSet<int> { 1, 2, 3 }.Discard(x => x > 10), Is.EqualTo(new HashSet<int> { 1, 2, 3 }));
     }
 
-    [Ignore]
     [Test]
     public void Discard_first_and_last()
     {
         Assert.That(new[] { 1, 2, 3 }.Discard(x => x % 2 != 0), Is.EqualTo(new[] { 2 }));
     }
 
-    [Ignore]
     [Test]
     public void Discard_neither_first_nor_last()
     {
         Assert.That(new List<int> { 1, 2, 3, 4, 5 }.Discard(x => x % 2 == 0), Is.EqualTo(new List<int> { 1, 3, 5 }));
     }
 
-    [Ignore]
     [Test]
     public void Discard_strings()
     {
@@ -94,7 +85,6 @@ public class StrainTest
         Assert.That(words.Discard(x => x.StartsWith("z")), Is.EqualTo("apple banana cherimoya".Split(' ')));
     }
 
-    [Ignore]
     [Test]
     public void Discard_arrays()
     {
