c7083f090d02d935d3637b78dd4c505101037662
Kent Chenery <kent@chenery.net.nz>
2015-06-12 23:08:35 +1200

    These delegates still confuse me a little

---
+++ csharp/accumulate/Accumulate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.Accumulate
+{
+    public static class Accumulator
+    {
+        public static IEnumerable<T> Accumulate<T>(this IEnumerable<T> source, Func<T, T> accumulation)
+        {
+            foreach(var item in source)
+            {
+                yield return accumulation(item);
+            }
+        }
+    }
+}
