6ab231b05ac9fa75a6abc2d6059b66d2cb975ef9
Kent Chenery <kent@chenery.net.nz>
2015-06-14 00:03:31 +1200

    Sure we have had a similar one before

---
+++ csharp/rna-transcription/Complement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.RNATranscription
+{
+    public static class Complement
+    {
+        private static Dictionary<char, char> DnaToRna = new Dictionary<char,char>(){
+            { 'G', 'C' }, 
+            { 'C', 'G' }, 
+            { 'T', 'A' }, 
+            { 'A', 'U' } 
+        };
+        private static Dictionary<char, char> RnaToDna = new Dictionary<char,char>(){ 
+            { 'C', 'G' }, 
+            { 'G', 'C' }, 
+            { 'A', 'T' }, 
+            { 'U', 'A' } 
+        };
+
+        public static string OfDna(string Dna)
+        {
+            return new string(Dna.ToCharArray().Select(x => DnaToRna[x]).ToArray());
+        }
+
+        public static string OfRna(string Rna)
+        {
+            return new string(Rna.ToCharArray().Select(x => RnaToDna[x]).ToArray());
+        }
+    }
+}
