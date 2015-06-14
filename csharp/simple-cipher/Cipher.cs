a47743c9c7c6f5a3b202fce68228603ec589cc8e
Kent Chenery <kent@chenery.net.nz>
2015-06-14 22:47:14 +1200

    Works...but probably still some improvements available

--- csharp/simple-cipher/Cipher.cs
+++ csharp/simple-cipher/Cipher.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Exercism.SimpleCipher
 {
     public class Cipher
     {
         private const int DEFAULT_KEY_LENGTH = 100;
+        private const bool ENCODE = true;
+        private const bool DECODE = false;
         private const string ALLOWED_KEY_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
         private static Random random = new Random();
 
@@ -23,61 +24,57 @@ namespace Exercism.SimpleCipher
             }
 
             Key = key;
-
-            Console.WriteLine("Cipher key: {0}", Key);
         }
 
-        public Cipher() : this(RandomStrings.Generate(ALLOWED_KEY_CHARACTERS, 100, 100, 1, random).First())
+        public Cipher()
+            : this(RandomStrings.Generate(ALLOWED_KEY_CHARACTERS, 100, 100, 1, random).First())
         {
         }
 
         public string Encode(string message)
         {
-            var encodedMessage = new StringBuilder();
-            var index = 0;
-
-            foreach(var letter in message)
-            {
-                encodedMessage.Append(EncodeShift(letter, index));
-                index ++;
-            }
-
-            return encodedMessage.ToString();
+            return CodeMessage(message, ENCODE);
         }
 
         public string Decode(string message)
         {
-            var decodedMessage = new StringBuilder();
+            return CodeMessage(message, DECODE);
+        }
+
+        private string CodeMessage(string message, bool direction)
+        {
+            var codedMessage = new StringBuilder();
+
             var index = 0;
 
             foreach (var letter in message)
             {
-                decodedMessage.Append(DecodeShift(letter, index));
+                codedMessage.Append(ShiftChar(letter, index, direction));
                 index++;
             }
 
-            return decodedMessage.ToString();
+            return codedMessage.ToString();
         }
 
-        private char EncodeShift(char letter, int index)
+        private char ShiftChar(char letter, int index, bool isEncode = true)
         {
             index = index % Key.Length; // Wrap around if needed
             int shift = Math.Abs((int)('a' - Key[index]));
 
-            return (char)((((int)letter - 97 + shift) % 26) + 97);
-        }
-
-        private char DecodeShift(char letter, int index)
-        {
-            index = index % Key.Length; // Wrap around if needed
-            int shift = 26 - Math.Abs((int)('a' - Key[index]));
+            if (!(isEncode))
+            {
+                shift = 26 - shift;
+            }
 
             return (char)((((int)letter - 97 + shift) % 26) + 97);
         }
     }
 
-    public static class RandomStrings {
-    
+    /// <summary>
+    /// Random string generator. Taken from: http://stackoverflow.com/questions/4616685/how-to-generate-a-random-string-and-specify-the-length-you-want-or-better-gene
+    /// </summary>
+    public static class RandomStrings
+    {
         public static IEnumerable<string> Generate(string allowedChars,
                                                     int minLength,
                                                     int maxLength,
