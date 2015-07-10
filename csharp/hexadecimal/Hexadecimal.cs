7b25e79a0a3e6ad40296aff817bd7d3432ac393f
Kent Chenery <kent@chenery.net.nz>
2015-07-10 20:44:20 +1200

    Hexadecimal

---
+++ csharp/hexadecimal/Hexadecimal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.hexadecimal
+{
+    public static class Hexadecimal
+    {
+        public static Dictionary<char, int> HexValues = new Dictionary<char,int> {
+            {'0', 0}, 
+            {'1', 1},
+            {'2', 2},
+            {'3', 3},
+            {'4', 4},
+            {'5', 5},
+            {'6', 6},
+            {'7', 7},
+            {'8', 8},
+            {'9', 9},
+            {'a', 10},
+            {'b', 11},
+            {'c', 12},
+            {'d', 13},
+            {'e', 14},
+            {'f', 15},
+        };
+
+        public static int ToDecimal(string hexString)
+        {
+            hexString = new string(hexString.ToLower().Reverse().ToArray());
+
+            int depth = 0;
+            int value = 0;
+            int total = 0;
+
+            foreach(var letter in hexString)
+            {
+                if(!TryParseHexValue(letter, depth, out value))
+                {
+                    total = 0;
+                    break;
+                }
+
+                total += value;
+                depth++;
+            }
+
+            return total;
+        }
+
+        private static bool TryParseHexValue(char letter, int depth, out int value)
+        {
+            value = 0;
+            if (!HexValues.ContainsKey(letter)) return false;
+
+            int factor = (int)Math.Pow(16, depth);
+            value = (factor * HexValues[letter]);
+            return true;
+        }
+    }
+}
--- csharp/hexadecimal/HexadecimalTest.cs
+++ csharp/hexadecimal/HexadecimalTest.cs
@@ -1,31 +1,31 @@
 using NUnit.Framework;
+using Exercism.hexadecimal;
 
 [TestFixture]
 public class HexadecimalTest
 {
     // change Ignore to false to run test case or just remove 'Ignore = true'
     [TestCase("1", Result = 1)]
-    [TestCase("c", Result = 12, Ignore = true)]
-    [TestCase("10", Result = 16, Ignore = true)]
-    [TestCase("af", Result = 175, Ignore = true)]
-    [TestCase("100", Result = 256, Ignore = true)]
-    [TestCase("19ace", Result = 105166, Ignore = true)]
-    [TestCase("19ace", Result = 105166, Ignore = true)]
+    [TestCase("c", Result = 12, Ignore = false)]
+    [TestCase("10", Result = 16, Ignore = false)]
+    [TestCase("af", Result = 175, Ignore = false)]
+    [TestCase("100", Result = 256, Ignore = false)]
+    [TestCase("19ace", Result = 105166, Ignore = false)]
+    [TestCase("19ace", Result = 105166, Ignore = false)]
     public int Hexadecimal_converts_to_decimal(string hexadecimal)
     {
         return Hexadecimal.ToDecimal(hexadecimal);
     }
 
-    [Ignore]
     [Test]
     public void Invalid_hexadecimal_is_decimal_0()
     {
         Assert.That(Hexadecimal.ToDecimal("carrot"), Is.EqualTo(0));
     }
 
-    [TestCase("000000", Result = 0, Ignore = true)]
-    [TestCase("ffffff", Result = 16777215, Ignore = true)]
-    [TestCase("ffff00", Result = 16776960, Ignore = true)]
+    [TestCase("000000", Result = 0, Ignore = false)]
+    [TestCase("ffffff", Result = 16777215, Ignore = false)]
+    [TestCase("ffff00", Result = 16776960, Ignore = false)]
     public int Octal_can_convert_formatted_strings(string hexidecimal)
     {
         return Hexadecimal.ToDecimal(hexidecimal);
